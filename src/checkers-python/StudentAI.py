a4e69151df32679f640864e16aa2070e569a12fa
Alex Espejo <aespejo102@gmail.com>
2025-02-12 16:48:53 -0800

    reset

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -1,8 +1,29 @@
 from random import randint
+import math
 from BoardClasses import Move
 from BoardClasses import Board
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
+class MCTSNode:
+    def __init__(self, game_state, parent=None, move=None):
+        self.game_state = game_state
+        self.parent = parent
+        self.move = move
+        self.children = []
+        self.visit_count = 0
+        self.win_count = 0
+        self.uct_value = float('inf')
+        self.terminal = False
+        self.simulated_outcome = None
+
+    def uct(self, exploration_weight=1.0):
+        if self.visit_count == 0:
+            return float('inf')
+        return (self.win_count / self.visit_count) + exploration_weight * (math.sqrt(math.log(self.parent.visit_count) / self.visit_count))
+
+    def add_child(self, child_node):
+        self.children.append(child_node)
+
 class StudentAI():
 
     def __init__(self,col,row,p):
@@ -14,11 +35,16 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
+
+    # def backpropagate(self,game_state):
+    #
+
     def get_move(self,move):
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
         else:
             self.color = 1
+        root = MCTSNode(self.board)
         moves = self.board.get_all_possible_moves(self.color)
         index = randint(0,len(moves)-1)
         inner_index =  randint(0,len(moves[index])-1)
