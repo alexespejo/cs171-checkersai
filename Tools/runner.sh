b7721403dc3c7fb1f483230af3f81b44593e5799
Alex Espejo <aespejo102@gmail.com>
2025-03-01 17:48:17 -0800

    lower runs

--- Tools/runner.sh
+++ Tools/runner.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
-for i in {1..5}
-do
-  python3 AI_Runner.py 8 8 3 l Sample_AIs/Poor_AI/main.py ../src/checkers-python/main.py
-done
-
+echo "Going First"
 for i in {1..5}
 do
   python3 AI_Runner.py 8 8 3 l ../src/checkers-python/main.py Sample_AIs/Poor_AI/main.py 
+done
+
+echo "Going Second"
+for i in {1..5}
+do
+  python3 AI_Runner.py 8 8 3 l Sample_AIs/Poor_AI/main.py ../src/checkers-python/main.py
 done
